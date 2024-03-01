3c971d9f157d6d35f3910c3863fce779d8df6be5
mawen12 <1181963012mw@gmail.com>
2024-03-01 15:41:19 +0800

    finish module ==> httpserver

---
+++ six-easeagent/httpserver/src/main/java/com/mawen/agent/httpserver/nano/AgentHttpHandler.java
@@ -0,0 +1,75 @@
+package com.mawen.agent.httpserver.nano;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import com.mawen.agent.httpserver.nanohttpd.protocols.http.IHTTPSession;
+import com.mawen.agent.httpserver.nanohttpd.protocols.http.NanoHTTPD;
+import com.mawen.agent.httpserver.nanohttpd.protocols.http.request.Method;
+import com.mawen.agent.httpserver.nanohttpd.protocols.http.response.IStatus;
+import com.mawen.agent.httpserver.nanohttpd.protocols.http.response.Response;
+import com.mawen.agent.httpserver.nanohttpd.protocols.http.response.Status;
+import com.mawen.agent.httpserver.nanohttpd.router.RouterNanoHTTPD;
+
+/**
+ * @author <a href="1181963012mw@gmail.com">mawen12</a>
+ * @since 2024/3/1
+ */
+public abstract class AgentHttpHandler extends RouterNanoHTTPD.DefaultHandler {
+
+	public abstract String getPath();
+
+	protected String text;
+	protected Set<Method> methods = new HashSet<>(Arrays.asList(Method.PUT, Method.POST));
+
+	@Override
+	public String getMimeType() {
+		return null;
+	}
+
+	@Override
+	public String getText() {
+		return this.text;
+	}
+
+	@Override
+	public IStatus getStatus() {
+		return Status.OK;
+	}
+
+	@Override
+	public Response get(RouterNanoHTTPD.UriResource uriResource, Map<String, String> urlParams, IHTTPSession session) {
+		return this.process(uriResource, urlParams, session);
+	}
+
+	public abstract Response process(RouterNanoHTTPD.UriResource uriResource, Map<String, String> urlParams, IHTTPSession session);
+
+	protected String buildRequestBody(IHTTPSession session) {
+		try {
+			Map<String, String> files = new HashMap<>();
+			Method method = session.getMethod();
+			if (!methods.contains(method)) {
+				return null;
+			}
+			session.parseBody(files);
+			String content = files.get("content");
+			if (content != null) {
+				Path path = Paths.get(content);
+				byte[] bytes = Files.readAllBytes(path);
+				return new String(bytes, StandardCharsets.UTF_8);
+			}
+			return files.get("postData");
+		}
+		catch (IOException | NanoHTTPD.ResponseException e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
---
+++ six-easeagent/httpserver/src/main/java/com/mawen/agent/httpserver/nano/AgentHttpServer.java
@@ -0,0 +1,40 @@
+package com.mawen.agent.httpserver.nano;
+
+import java.io.IOException;
+import java.util.List;
+
+import com.mawen.agent.httpserver.nanohttpd.router.RouterNanoHTTPD;
+
+/**
+ * @author <a href="1181963012mw@gmail.com">mawen12</a>
+ * @since 2024/3/1
+ */
+public class AgentHttpServer extends RouterNanoHTTPD {
+
+	public static String JSON_TYPE = "application/json";
+
+	public AgentHttpServer(int port) {
+		super(port);
+		this.addMappings();
+		Runtime.getRuntime().addShutdownHook(new Thread(this::stop));
+	}
+
+	public void addHttpRoutes(List<AgentHttpHandler> agentHttpHandlers) {
+		for (AgentHttpHandler agentHttpHandler : agentHttpHandlers) {
+			this.addRoute(agentHttpHandler.getPath(), agentHttpHandler.getClass());
+		}
+	}
+
+	public void addHttpRoute(AgentHttpHandler handler) {
+		this.addRoute(handler.getPath(), handler.getClass());
+	}
+
+	public void startServer() {
+		try {
+			this.start(5000, true);
+		}
+		catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
